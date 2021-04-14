2216b787d022645a2afe92e3b336795fd982f342
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-14 08:29:28 -0700

    adding the initial files, fizz_buzz.py script still in work

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,4 +1,12 @@
 def fizz_buzz(n):
+    for i in range(1,101):
+        if i % 3 == 0 and not i%5 == 0:
+            print('Fizz')
+        print(i)
+        
+        pass #end of for loop
+
+
     pass
 
 
