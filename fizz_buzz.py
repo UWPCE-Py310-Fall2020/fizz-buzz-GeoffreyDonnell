d5aea4a029214f532cf974e781e4da00204ec729
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-14 09:24:31 -0700

    fizz_buzz.py script is ready for review

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,14 +1,13 @@
 def fizz_buzz(n):
     for i in range(1,101):
+        if not i % 3 == 0 and not i % 5 ==0:
+            print (i)
         if i % 3 == 0 and not i%5 == 0:
             print('Fizz')
-        print(i)
-        
-        pass #end of for loop
+        if not i % 3 == 0 and i % 5 == 0:
+            print('Buzz')
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+pass #end of function block
 
-
-    pass
-
-
-if __name__ == "__main__":
-    fizz_buzz(100)
+fizz_buzz(100)
